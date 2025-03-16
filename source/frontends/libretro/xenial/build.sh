5076c021effdb6002100e6cd943db3107aee5d84
Andrea Odetti <mariofutire@gmail.com>
2025-03-16 17:07:25 +0000

    Add xenial libretro build script.
    https://github.com/audetto/AppleWin/issues/253
    Signed-off-by: Andrea Odetti <mariofutire@gmail.com>

---
+++ source/frontends/libretro/xenial/build.sh
@@ -0,0 +1,8 @@
+# docker run -it --rm -v "AppleWin:/build" git.libretro.com:5050/libretro-infrastructure/libretro-build-amd64-ubuntu:xenial-gcc9 bash
+apt-get update -qy
+apt-get upgrade -qy
+git clone https://github.com/audetto/AppleWin.git --depth=1
+cd AppleWin
+apt-get -qy install $(cat source/frontends/libretro/xenial/packages.txt)
+cmake -DBUILD_LIBRETRO=ON -B build -G Ninja
+cmake --build build
